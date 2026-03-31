ceb425b61732748f70330e80e878255913ec9aaf
Lorenzzo Guerra <lorenzzo62215446@alunos.sc.senac.br>
2026-03-30 21:28:10 -0300

    menu

---
+++ menu =1.js
@@ -0,0 +1,18 @@
+menu =1
+//solicita a opção do usuário
+let opção = 1;
+ 
+//verifica a opção escolhida
+switch (opção) {
+    case 1:
+        console.log("você escolheu coca-cola");
+        break;
+    case 2:
+        console.log("você escolheu pepsi");
+        break;  
+    case 3:
+        console.log("você escolheu fanta");
+        break;
+        default:
+        console.log("opção inválida");
+}
